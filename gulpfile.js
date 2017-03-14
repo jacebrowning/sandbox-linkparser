894cdd86f9084e220d13b38596191177e6e0f892
Jace Browning <jacebrowning@gmail.com>
2017-03-14 19:42:51 -0400

    Rename generated JavaScript files ot *.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 // requirements
 
 var gulp = require('gulp');
-var gulpBrowser = require("gulp-browser");
+var gulpBrowser = require('gulp-browser');
+var rename = require('gulp-rename');
+var size = require('gulp-size');
 var reactify = require('reactify');
 var del = require('del');
-var size = require('gulp-size');
 
 
 // tasks
@@ -12,6 +13,7 @@ var size = require('gulp-size');
 gulp.task('transform', function () {
   var stream = gulp.src('./project/static/scripts/jsx/*.jsx')
     .pipe(gulpBrowser.browserify({transform: ['reactify']}))
+    .pipe(rename({extname: '.js'}))
     .pipe(gulp.dest('./project/static/scripts/js/'))
     .pipe(size());
   return stream;
